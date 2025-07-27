369150de10a9081214b5c67d634a3d6a553e2b6f
K.Yuzen <146518153+K-Yuzen-GitHub@users.noreply.github.com>
2025-07-27 21:52:44 +0900

    Add files via upload

---
+++ apk/Policy Management System.bat
@@ -0,0 +1,39 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQIAC1t7XBCLMWKgB6EV5+a72+WIri0=
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQIDMQhASQni
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFJ2Gn4N27Oha5jP+KG8fOdajrU4IWecxbJznyvqLOOVz
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge /v InPrivateModeAvailability /f
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge /v AllowDeletingBrowserHistory /f
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome /v IncognitoModeAvailability /f
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome /v AllowDeletingBrowserHistory /f
+exit
